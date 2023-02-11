2013e491b64e037a796f2fab3df614c359b4c020
Maris Erts <mariserts84@gmail.com>
2023-02-11 12:49:39 +0200

    Added test for authenticate sign in and register, 71%

--- junkyard_api/tests/test_viewsets_authenticate.py
+++ junkyard_api/tests/test_viewsets_authenticate.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from typing import Type
+from unittest.mock import patch
+
 from .test_base import BaseTestCase
 
 
@@ -6,27 +9,67 @@ class AuthenticateViewSetTestCase(
     BaseTestCase
 ):
 
-    pass
+    def setUp(self):
+        super().setUp()
+        self.mock_request = patch(
+            'junkyard_api.viewsets.authenticate.requests.post'
+        )
+        self.mock_post = self.mock_request.start()
 
-    # def test_sign_in(
-    #     self: Type
-    # ):
-    #
-    #     request = self.client.post(
-    #         '/api/authenticate/sign-in/',
-    #         {
-    #             'email': self.user_one_email,
-    #             'password': self.user_one_password
-    #         },
-    #         format='json'
-    #     )
-    #
-    #     self.assertEquals(
-    #         request.status_code,
-    #         201
-    #     )
-    #
-    #     self.assertIn(
-    #         'user',
-    #         request.json()
-    #     )
+    def tearDown(self):
+        self.mock_request.stop()
+        super().tearDown()
+
+    def test_register(
+        self: Type
+    ):
+
+        self.mock_post.return_value.status_code = 201
+        self.mock_post.return_value.ok = True
+        self.mock_post.return_value.json.return_value = {}
+
+        request = self.client.post(
+            '/api/authenticate/register/',
+            {
+                'email': 'user_four@test.case',
+                'password': 'abcd1234!'
+            },
+            format='json'
+        )
+
+        self.assertEquals(
+            request.status_code,
+            201
+        )
+
+        self.assertIn(
+            'user',
+            request.json()
+        )
+
+    def test_sign_in(
+        self: Type
+    ):
+
+        self.mock_post.return_value.status_code = 201
+        self.mock_post.return_value.ok = True
+        self.mock_post.return_value.json.return_value = {}
+
+        request = self.client.post(
+            '/api/authenticate/sign-in/',
+            {
+                'email': self.user_one_email,
+                'password': self.user_one_password
+            },
+            format='json'
+        )
+
+        self.assertEquals(
+            request.status_code,
+            201
+        )
+
+        self.assertIn(
+            'user',
+            request.json()
+        )
