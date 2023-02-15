c2945e2f5069d8bf4105cbefb9239cdddf632f44
Maris Erts <mariserts84@gmail.com>
2023-02-15 09:48:44 +0200

    Added tests for item cration, updated permissions for safe methods, 79%

---
+++ junkyard_api/tests/test_utils_get_projects_tenants_items_url.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from typing import Type
+
+from django.test import TestCase
+
+from ..utils.urls import get_projects_tenants_items_url
+
+
+class UtilsUrlsGetProjectsTenantsItemsUrlTestCase(
+    TestCase
+):
+
+    def test_one(
+        self: Type
+    ) -> None:
+
+        url = get_projects_tenants_items_url(
+            1,
+            1,
+        )
+
+        self.assertEquals(
+            url,
+            '/api/projects/1/tenants/1/items/'
+        )
+
+        url = get_projects_tenants_items_url(
+            1,
+            1,
+            item_pk=1
+        )
+
+        self.assertEquals(
+            url,
+            '/api/projects/1/tenants/1/items/1/'
+        )
